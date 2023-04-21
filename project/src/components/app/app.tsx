4b98f1c11324c83300f0b4dd0f08009351255fdf
shestopalovmatvey <shestopalov.myu@edu.spbstu.ru>
2023-04-21 19:22:43 +0300

    module2_1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,8 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import WelcomePage from '../../pages/welcome-page/welcomePage';
+import { WelcomePageProps } from '../../pages/welcome-page/welcomePage';
+
+function App(props: WelcomePageProps): JSX.Element {
+  return <WelcomePage {...props}/>;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -6,8 +6,14 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const promoFilm = {
+  filmTitle: 'The Grand Budapest Hotel',
+  genreFilm: 'Drama',
+  releaseYearFilm: 2014,
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App nameFilm={promoFilm.filmTitle} genreFilm={promoFilm.genreFilm} releaseYearFilm={promoFilm.releaseYearFilm}/>
   </React.StrictMode>,
 );
